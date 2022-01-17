3576f9a5389acd3492cb1f81c1a6fd193682dc9e
Thanwa1121 <97673057+Thanwa1121@users.noreply.github.com>
2022-01-17 10:43:18 +0700

    Add files via upload

---
+++ love.py
@@ -0,0 +1,75 @@
+import requests
+import time
+import threading
+from threading import Thread
+print ("")
+phone = input("เบอร์ : ")
+NUM = int(input("จำนวน : "))
+
+def api():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : LOTUSS")
+	
+def api2():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : AURORA")
+	
+def api3():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : MOVDIER")
+	
+def api4():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : BOOT")
+	
+def api5():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : RUOK")
+	
+def api6():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : OTP ")
+	
+def api7():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : SMS")
+	
+def api8():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : COVID")
+	
+def api9():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : ไอตู่")
+	
+def api10():
+    print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : PHONE")
+	
+def api11():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : KFC")
+	
+def api12():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : ROV")
+	
+def api13():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : FREEFIRE")
+	
+def api14():
+	print ("𝑨𝑻𝑻𝑨𝑪𝑲 𝑺𝑴𝑺 : PUBG")
+
+	
+for i in range(NUM):
+	time.sleep(0.00000000000000000000001)
+	threading.Thread(target=api).start()
+	threading.Thread(target=api2).start()
+	threading.Thread(target=api3).start()
+	threading.Thread(target=api4).start()
+	threading.Thread(target=api5).start()
+	threading.Thread(target=api6).start()
+	threading.Thread(target=api7).start()
+	threading.Thread(target=api8).start()
+	threading.Thread(target=api9).start()
+	threading.Thread(target=api10).start()
+	threading.Thread(target=api11).start()
+	threading.Thread(target=api12).start()
+	threading.Thread(target=api13).start()
+	threading.Thread(target=api14).start()
+              threading.Thread(target=api15).start()
+             threading.Thread(target=api16).start()
+             threading.Thread(target=api17).start()
+             threading.Thread(target=api18).start()
+             threading.Thread(target=api19).start()
+             threading.Thread(target=api20).start()
+             threading.Thread(target=api21).start()
+             threading.Thread(target=api22).start()
